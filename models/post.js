d7ce54e6d2a8ea403e3faec6541728722939f58c
kangyoukyeong <jinny3685@hyundai.com>
2022-09-06 16:41:58 +0900

    following, post with image, searching by hashtag

--- models/post.js
+++ models/post.js
@@ -25,6 +25,8 @@ module.exports = class Post extends Sequelize.Model {
     }
 
     static associate(db) {
+        db.Post.belongsTo(db.User);
+
         db.Post.belongsToMany(db.Hashtag, {
             through: 'PostHashtag'
         });
---
+++ routes/authjwt.js
@@ -0,0 +1,15 @@
+// 나중에 다시 해보기
+
+/*
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'M';
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/jwtlogin', function(req, res, next){
+    console.log("jwt sign");
+    const { id, email, nick, password } = req.body;
+});
+ */
